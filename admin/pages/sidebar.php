aca179cdff556a24883771e3e1a63740cadaf318
Lngqminh <lngqminh@gmail.com>
2024-11-23 14:13:43 +0700

    Create admin

---
+++ admin/pages/sidebar.php
@@ -0,0 +1,82 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+    <div class="app-brand demo">
+        <a href="index.php" class="app-brand-link">
+            <span class="app-brand-logo demo me-1">
+                <span style="color: var(--bs-primary)">
+                    <img width="150" height="60" style="margin-top: 30px" src="../../assets/img/logo.png">
+                        
+                </span>
+            </span>
+            <!-- <span class="app-brand-text demo menu-text fw-semibold ms-2">LACTHANBUS</span> -->
+        </a>
+        <!-- <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
+            <i class="menu-toggle-icon d-xl-block align-middle"></i>
+        </a> -->
+    </div>
+    <div class="menu-inner-shadow"></div>
+    <ul class="menu-inner py-1">
+        <!-- Dashboards -->
+        <li class="menu-item">
+            <!-- menu-toggle -->
+            <a href="index.php" class="menu-link">
+                <i class="menu-icon tf-icons ri-home-smile-line"></i>
+                <div data-i18n="Dashboards">Dashboards</div>
+            </a>
+        </li>
+        <!-- Forms & Tables -->
+        <li class="menu-header fw-medium mt-4"><span class="menu-header-text">DỮ LIỆU</u></span></li>
+        <!-- Tables -->
+        <li class="menu-item">
+            <a href="benxe_manager.php" class="menu-link">
+                <i class="menu-icon tf-icons ri-table-alt-line"></i>
+                <div data-i18n="Tables">Bến Xe</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="lessons_manage.php" class="menu-link">
+                <i class="menu-icon tf-icons ri-table-alt-line"></i>
+                <div data-i18n="Tables">Hành Khách</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="solutions_manager.php" class="menu-link">
+                <i class="menu-icon tf-icons ri-table-alt-line"></i>
+                <div data-i18n="Tables">Tuyến Xe</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="quiz_manager.php" class="menu-link">
+                <i class="menu-icon tf-icons ri-table-alt-line"></i>
+                <div data-i18n="Tables">Xe</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="essay_manager.php" class="menu-link">
+                <i class="menu-icon tf-icons ri-table-alt-line"></i>
+                <div data-i18n="Tables">Loại Xe</div>
+            </a>
+        </li>
+        <!-- QUẢN LÝ -->
+        <li class="menu-header fw-medium mt-4"><span class="menu-header-text">QUẢN LÝ</u></span></li>
+        <li class="menu-item">
+            <a href="admin_contact.php" class="menu-link">
+                <i class="menu-icon tf-icons ri-table-alt-line"></i>
+                <div data-i18n="Tables">Quản Lý Lịch Trình</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="admin_contact.php" class="menu-link">
+                <i class="menu-icon tf-icons ri-table-alt-line"></i>
+                <div data-i18n="Tables">Quản Lý Phản Hồi</div>
+            </a>
+        </li>
+        <!-- HÓA ĐƠN -->
+        <li class="menu-header fw-medium mt-4"><span class="menu-header-text">HÓA ĐƠN</u></span></li>
+        <li class="menu-item">
+            <a href="admin_contact.php" class="menu-link">
+                <i class="menu-icon tf-icons ri-table-alt-line"></i>
+                <div data-i18n="Tables">Thống Kê Hóa Đơn</div>
+            </a>
+        </li>
+    </ul>
+</aside>
