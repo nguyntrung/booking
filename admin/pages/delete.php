d11a957de9f9f5c786480b96e9f26a24c787c400
Taiphan <taiphan@i-soft.com.vn>
2024-11-27 01:38:10 +0700

    update source and delete function
    Sử dụng theo delete của Tài làm (đã tối ưu)
    không hiểu liên hệ tài đẹp trai

---
+++ admin/pages/delete.php
@@ -0,0 +1,46 @@
+<?php
+// update_enableflag.php
+
+// Kết nối đến cơ sở dữ liệu
+include '../../database/db.php';
+
+
+// Kiểm tra nếu có dữ liệu được gửi đến
+if (isset($_POST['type']) &&isset($_POST['id']) && isset($_POST['enableflag'])) {
+    $table = $conn->real_escape_string($_POST['type']);
+    switch ($table) {
+        case 'nhanvien':
+            $column ='MaNV';
+            break;
+        case 'hanhkhach':
+            $column = 'MaHK';
+            break;
+        case 'tuyenxe':
+            $column = 'MaTuyenXe';
+            break;
+        case 'loaixe':
+            $column = 'MaLoaiXe';
+            break;
+        case 'xe':
+            $column = 'MaXe';
+            break;
+        default:
+            die('Error!');
+    }
+    $maNV = $_POST['id'];
+    $enableflag = $_POST['enableflag'];
+    $sql = "UPDATE {$table} SET enableflag = ? WHERE {$column} = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("is", $enableflag, $maNV);
+    if ($stmt->execute()) {
+        echo "Cập nhật thành công";
+    } else {
+        echo "Lỗi cập nhật";
+    }
+
+    $stmt->close();
+    $conn->close();
+} else {
+    echo "Dữ liệu không hợp lệ";
+}
+?>
